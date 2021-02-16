1d72a4151a100b7653380b8c0458576c06327515
Christophe Meessen <christophe@meessen.net>
2021-02-16 15:19:47 +0100

    initial import

---
+++ src/hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+
+// hello return a heap allocated string containing the name appended 
+// to "hello " and followed by "!".
+char *hello(char *name) {
+    char *buf = malloc(7+strlen(name));
+    sprintf(buf, "hello %s!", name);
+    return buf;
+}
