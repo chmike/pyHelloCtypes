8e2828ab246c867a553505807bf342190849c4c7
Christophe Meessen <christophe@meessen.net>
2021-02-17 15:57:58 +0100

    make hello __init__.py portable on macOS

--- hello/__init__.py
+++ hello/__init__.py
@@ -2,20 +2,22 @@
 Simple hello example using an external C library.
 """
 import ctypes as ct
+import platform
 import glob
 import os
 
 _lib = ct.cdll.LoadLibrary(glob.glob(os.path.join(__path__[0],'ext.*.so'))[0])
 _lib.hello.argtypes = [ct.c_char_p]
 _lib.hello.restype = ct.c_void_p
-_libc = ct.cdll.LoadLibrary("libc.so.6")
+_libc = ct.cdll.LoadLibrary(\
+    "libc.{}".format("so.6" if platform.uname()[0] != "Darwin" else "dylib"))
 _libc.free.argtypes = [ct.c_void_p]
 
 def hello(name: str) -> str:
     """
-    hello(name) returns "hello "+name+"!". 
+    hello(name) returns "hello "+name+"!".
 
-    An exception is thrown when the argument is not a string. 
+    An exception is thrown when the argument is not a string.
     """
     if not isinstance(name, str):
         raise TypeError("hello() argument must be of type string")
